40c864d034e7844c2abd97d9200cb84dd7ed0025
Kam Kennedy <kameronk013@gmail.com>
2024-01-03 15:44:35 -0700

    progress on genres controller

--- app/controllers/Api/V1/GenresController.rb
+++ app/controllers/Api/V1/GenresController.rb
@@ -1,6 +1,12 @@
 class Api::V1::GenresController < ApplicationController
   def index
-    @genres = Genre.all
-    render json: @genres
+    conn = Faraday.new(url: "https://api.spotify.com/") do |faraday|
+      faraday.headers["Bearer"] = Rails.application.credentials.spotify[:token]
+    end
+
+    response = conn.get("v1/recommendations/available-genre-seeds")
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    @members = json[:results]
   end
 end
--- spec/requests/api/v1/genres_request_spec.rb
+++ spec/requests/api/v1/genres_request_spec.rb
@@ -7,5 +7,17 @@ describe "Genres API" do
     get '/api/v1/genres'
 
     expect(response).to be_successful
+
+    genres = JSON.parse(response.body, symbolize_names: true)
+
+    expect(genres.count).to eq(10)
+
+    genres.each do |genre|
+      expect(genre).to have_key(:id)
+      expect(genre[:id]).to be_an(Integer)
+
+      expect(genre).to have_key(:name)
+      expect(genre[:name]).to be_a(String)
+    end
   end
 end
