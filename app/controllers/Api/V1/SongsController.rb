a41ebfc0ce5c411b1015916a27e012a5b9d5abeb
Scott DeVoss <scottdevoss@yahoo.com>
2024-01-05 10:44:28 -0700

    added sad path testing

--- app/controllers/Api/V1/SongsController.rb
+++ app/controllers/Api/V1/SongsController.rb
@@ -24,14 +24,24 @@ class Api::V1::SongsController < ApplicationController
       end
       #Make endpoint connection
       response = conn.get("v1/recommendations?limit=#{num_songs}&seed_genres=#{params[:genre]}&target_tempo=#{tempo}")
-      json = JSON.parse(response.body, symbolize_names: true)
-      
-      json[:tracks].each do |track|
-        track[:artists].each do |artist|
-          artist[:name]
-        end
-        track[:name]
+      # json = JSON.parse(response.body, symbolize_names: true)
+
+      if response.status == 401
+        render json: { error: { message: "Invalid access token", status: 401 } }
+      elsif response.status == 404
+        render json: { error: { message: "Not Found", status: 404 } }
+      elsif response.status != 200
+        render json: { error: { message: "Unexpected error", status: response.status } }
+      else
+        json = JSON.parse(response.body, symbolize_names: true)
       end
+      
+      # json[:tracks].each do |track|
+      #   track[:artists].each do |artist|
+      #     artist[:name]
+      #   end
+      #   track[:name]
+      # end
       # require 'pry'; binding.pry
     end 
   end
--- spec/requests/api/v1/songs_selection_spec.rb
+++ spec/requests/api/v1/songs_selection_spec.rb
@@ -35,4 +35,48 @@ describe "Song Selection" do
         end
     end
   end 
+
+  it "errors without a token" do
+    json_response = File.read('spec/fixtures/songs_selection/songs_no_token.json')
+
+    stub_request(:get, "https://api.spotify.com/v1/recommendations?limit=10&seed_genres=pop&target_tempo=140").
+         with(
+           headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'Authorization'=>'Bearer',
+          'User-Agent'=>'Faraday v2.8.1'
+           }).
+         to_return(status: 401, body: json_response, headers: {})
+
+    get "/api/v1/songs?bearer=&&workout=HIIT&&genre=pop"
+
+    expect(response).to be_successful
+
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+    expect(parsed_response[:error][:message]).to eq("No token provided")  
+    expect(parsed_response[:error][:status]).to eq(401)  
+  end
+
+  it "errors with a bad token" do
+    json_response = File.read('spec/fixtures/songs_selection/songs_bad_token.json')
+
+    stub_request(:get, "https://api.spotify.com/v1/recommendations?limit=10&seed_genres=pop&target_tempo=140").
+    with(
+      headers: {
+     'Accept'=>'*/*',
+     'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+     'Authorization'=>'Bearer 1235',
+     'User-Agent'=>'Faraday v2.8.1'
+      })
+    .to_return(status: 401, body: json_response, headers: {})
+
+    get "/api/v1/songs?bearer=1235&&workout=HIIT&&genre=pop"
+
+    expect(response).to be_successful
+    
+    parsed_response = JSON.parse(json_response, symbolize_names: true)
+    expect(parsed_response[:error][:message]).to eq("Invalid access token")  
+    expect(parsed_response[:error][:status]).to eq(401)  
+  end
 end 
