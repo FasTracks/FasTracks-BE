d2efc3239cfbf86050e31f492b4e55f38a143684
Kam Kennedy <kameronk013@gmail.com>
2024-01-08 13:40:55 -0700

    YAGNI refactor

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  # get "/api/v1/genres", to: "api/v1/genres#index"
   post "/api/v1/playlists", to: "api/v1/playlists#generate"
 end
